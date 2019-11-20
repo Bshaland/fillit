d139dbdee4512df66b450d1e10b2cd4bedf069a6
Benedar Shalanda <bshaland@oa-n5.21-school.ru>
2019-11-20 16:35:19 +0300

    This is only start of our work, I did main(not finished yet), and header file(will pull data in it)

---
+++ fillit_headers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit_headers.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshaland <bshaland@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 15:26:51 by bshaland          #+#    #+#             */
+/*   Updated: 2019/11/20 16:26:40 by bshaland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_HEADERS_H
+
+# define FILLIT_HEADERS_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
